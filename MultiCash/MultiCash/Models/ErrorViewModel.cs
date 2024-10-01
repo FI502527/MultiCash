329cbc54853e62682f9512861d7f21e409f9134e
FI502527 <502527@student.fontys.nl>
2024-10-01 10:51:49 +0200

    Project start

---
+++ MultiCash/MultiCash/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiCash.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
