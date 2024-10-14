64cbe229a96b7335e11b4de6c3b6da195d0fb00a
FI502527 <502527@student.fontys.nl>
2024-10-14 21:25:39 +0200

    Pc push naar laptop

---
+++ MultiCash/Data/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.DTO;
+using Microsoft.Data.SqlClient;
+
+namespace Data
+{
+	public class UserRepository
+	{
+		public UserDTO LoadUserById(int id)
+		{
+			Connection conn = new();
+			using (SqlConnection sqlConnection = conn.GetConnection())
+			{
+				SqlCommand command = new SqlCommand($"SELECT * FROM User WHERE id = {id};", sqlConnection);
+				UserDTO user = new UserDTO();
+				sqlConnection.Open();
+				SqlDataReader DataReader = command.ExecuteReader();
+				if (DataReader.HasRows)
+				{
+					while (DataReader.Read())
+					{
+						user.Id = DataReader.GetInt32(0);
+						user.Email = DataReader.GetString(1);
+						user.Password = DataReader.GetString(2);
+					}
+				}
+				return user;
+			}
+		}
+	}
+}
